5a5265bf0d286edadddfa0b33c0d9778a4515c6c
ThanhTinhHenry <ThanhTinh.Henry@gmail.com>
2025-10-12 12:42:20 +0700

    individualChatPage: own message card & reply message card

--- lib/Pages/IndividualChatPage.dart
+++ lib/Pages/IndividualChatPage.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app_flutter/CustomUI/OwnMessagesCard.dart';
+import 'package:chat_app_flutter/CustomUI/ReplyMesageCard.dart';
 import 'package:chat_app_flutter/Models/ChatModel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -63,214 +65,256 @@ class _IndividualChatPageState extends State<IndividualChatPage> {
         // Sau khi đóng emoji/keyboard, lần back tiếp theo mới pop route.
       },
 
-      child: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(60),
-          child: AppBar(
-            leadingWidth: 80,
-            // titleSpacing: 0,
-            backgroundColor: Color(0xFF075E54),
-            leading: InkWell(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.arrow_back_ios, size: 24),
-                  CircleAvatar(
-                    radius: 20,
-                    backgroundColor: Colors.blueAccent,
-                    child: SvgPicture.asset(
-                      (widget.chatModel.isGroup ?? false)
-                          ? 'assets/svg/group.svg'
-                          : 'assets/svg/person.svg',
-                      width: 28,
-                      height: 28,
-                      // color: Color(#fff),
+      child: Stack(
+        children: [
+          Image.asset(
+            "assets/whatsapp_Back.png",
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+          ),
+          Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: PreferredSize(
+              preferredSize: Size.fromHeight(60),
+              child: AppBar(
+                leadingWidth: 80,
+                // titleSpacing: 0,
+                backgroundColor: Color(0xFF075E54),
+                leading: InkWell(
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.arrow_back_ios, size: 24),
+                      CircleAvatar(
+                        radius: 20,
+                        backgroundColor: Colors.blueAccent,
+                        child: SvgPicture.asset(
+                          (widget.chatModel.isGroup ?? false)
+                              ? 'assets/svg/group.svg'
+                              : 'assets/svg/person.svg',
+                          width: 28,
+                          height: 28,
+                          // color: Color(#fff),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                title: InkWell(
+                  onTap: () {},
+                  child: Container(
+                    margin: EdgeInsets.all(5),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.chatModel.name ?? "Unknown",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ), // !fix
+                        Text(
+                          'last seen today at 07:30',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.lightGreen,
+                          ),
+                        ),
+                      ],
                     ),
+                  ),
+                ),
+                actions: [
+                  IconButton(onPressed: () {}, icon: Icon(Icons.call)),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.video_call)),
+                  PopupMenuButton<String>(
+                    // TODO: menu cho phan 3 cham
+                    onSelected: (value) {
+                      print(value);
+                    },
+                    itemBuilder: (BuildContext contexts) {
+                      return [
+                        PopupMenuItem(
+                          value: "View Contact",
+                          child: Text('View Contact'),
+                        ),
+                        PopupMenuItem(
+                          value: "Media, links, docs",
+                          child: Text('Media, links, docs'),
+                        ),
+                        PopupMenuItem(value: "Search", child: Text('Search')),
+                        PopupMenuItem(
+                          value: "WhatsApp Webs",
+                          child: Text('WhatsApp Webs'),
+                        ),
+                        PopupMenuItem(
+                          value: "Mute Notification",
+                          child: Text('Mute Notification'),
+                        ),
+                        PopupMenuItem(
+                          value: "Wallpaper",
+                          child: Text('Wallpaper'),
+                        ),
+                      ];
+                    },
                   ),
                 ],
               ),
             ),
-            title: InkWell(
-              onTap: () {},
-              child: Container(
-                margin: EdgeInsets.all(5),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      widget.chatModel.name ?? "Unknown",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ), // !fix
-                    Text(
-                      'last seen today at 07:30',
-                      style: TextStyle(fontSize: 12, color: Colors.lightGreen),
+            body: Column(
+              children: [
+                // 1) Danh sách tin nhắn chiếm phần còn lại
+                Expanded(
+                  child: Container(
+                    height: MediaQuery.of(context).size.height - 140,
+                    child: ListView(
+                      // reverse: true, // nếu muốn neo đáy
+                      padding: const EdgeInsets.only(bottom: 8),
+                      shrinkWrap: true,
+                      children: [
+                        OwnMessageCard(),
+                        ReplyMessageCard(),
+                        OwnMessageCard(),
+                        ReplyMessageCard(),
+                        OwnMessageCard(),
+                        ReplyMessageCard(),
+                        OwnMessageCard(),
+                        ReplyMessageCard(),
+                        OwnMessageCard(),
+                        ReplyMessageCard(),
+                        OwnMessageCard(),
+                        ReplyMessageCard(),
+                        OwnMessageCard(),
+                        ReplyMessageCard(),
+                        OwnMessageCard(),
+                        ReplyMessageCard(),
+                        OwnMessageCard(),
+                        ReplyMessageCard(),
+                        OwnMessageCard(),
+                        ReplyMessageCard(),
+                      ],
                     ),
-                  ],
-                ),
-              ),
-            ),
-            actions: [
-              IconButton(onPressed: () {}, icon: Icon(Icons.call)),
-              IconButton(onPressed: () {}, icon: Icon(Icons.video_call)),
-              PopupMenuButton<String>(
-                // TODO: menu cho phan 3 cham
-                onSelected: (value) {
-                  print(value);
-                },
-                itemBuilder: (BuildContext contexts) {
-                  return [
-                    PopupMenuItem(
-                      value: "View Contact",
-                      child: Text('View Contact'),
-                    ),
-                    PopupMenuItem(
-                      value: "Media, links, docs",
-                      child: Text('Media, links, docs'),
-                    ),
-                    PopupMenuItem(value: "Search", child: Text('Search')),
-                    PopupMenuItem(
-                      value: "WhatsApp Webs",
-                      child: Text('WhatsApp Webs'),
-                    ),
-                    PopupMenuItem(
-                      value: "Mute Notification",
-                      child: Text('Mute Notification'),
-                    ),
-                    PopupMenuItem(value: "Wallpaper", child: Text('Wallpaper')),
-                  ];
-                },
-              ),
-            ],
-          ),
-        ),
-        body: Column(
-          children: [
-            // 1) Danh sách tin nhắn chiếm phần còn lại
-            Expanded(
-              child: ListView(
-                // reverse: true, // nếu muốn neo đáy
-                padding: const EdgeInsets.only(bottom: 8),
-              ),
-            ),
-
-            // 2) Composer (thanh nhập). Khi emoji mở => không cần SafeArea bottom cho composer
-            SafeArea(
-              top: false,
-              left: false,
-              right: false,
-              bottom: !_emojiShowing,
-              child: Padding(
-                padding: const EdgeInsets.only(bottom: 4),
-                child: Row(
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width - 65,
-                      child: Card(
-                        margin: const EdgeInsets.only(left: 6, right: 2),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(25),
-                        ),
-                        child: TextFormField(
-                          controller: _textController,
-                          focusNode: _focusNode,
-                          textAlignVertical: TextAlignVertical.center,
-                          keyboardType: TextInputType.multiline,
-                          maxLines: 5,
-                          minLines: 1,
-                          onTap: () {
-                            if (_emojiShowing)
-                              setState(() => _emojiShowing = false);
-                          },
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Type a message",
-                            prefixIcon: IconButton(
-                              onPressed: _toggleEmoji,
-                              icon: const Icon(Icons.emoji_emotions),
-                            ),
-                            suffixIcon: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                IconButton(
-                                  onPressed: () {
-                                    showModalBottomSheet(
-                                      backgroundColor: Colors.transparent,
-                                      context: context,
-                                      builder: (builder) => bottomSheet(),
-                                    );
-                                  },
-                                  icon: const Icon(Icons.attach_file),
-                                ),
-                                IconButton(
-                                  onPressed: () {},
-                                  icon: const Icon(Icons.camera_alt),
-                                ),
-                              ],
-                            ),
-                            contentPadding: const EdgeInsets.all(5),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(right: 2, left: 2),
-                      child: CircleAvatar(
-                        radius: 25,
-                        backgroundColor: const Color(0xFF128C7E),
-                        child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.mic, color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            // 3) Emoji panel ở DƯỚI composer, không chồng lấn
-            Offstage(
-              offstage:
-                  !_emojiShowing ||
-                  viewInsets > 0, // nếu bàn phím mở thì ẩn emoji
-              child: SafeArea(
-                top: false,
-                left: false,
-                right: false,
-                bottom: true, // kê lên trên gesture bar
-                child: EmojiPicker(
-                  textEditingController: _textController,
-                  scrollController: _emojiScrollController,
-                  config: Config(
-                    height: 280,
-                    checkPlatformCompatibility: true,
-                    emojiViewConfig: EmojiViewConfig(
-                      emojiSizeMax:
-                          28 *
-                          (foundation.defaultTargetPlatform ==
-                                  TargetPlatform.iOS
-                              ? 1.2
-                              : 1.0),
-                    ),
-                    // categoryViewConfig: const CategoryViewConfig(
-                    //   initCategory: Category.SMILEYS,
-                    //   recentTabBehavior: RecentTabBehavior.NONE,
-                    // ),
                   ),
                 ),
-              ),
+
+                // 2) Composer (thanh nhập). Khi emoji mở => không cần SafeArea bottom cho composer
+                SafeArea(
+                  top: false,
+                  left: false,
+                  right: false,
+                  bottom: !_emojiShowing,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 4),
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width - 65,
+                          child: Card(
+                            margin: const EdgeInsets.only(left: 6, right: 2),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                            child: TextFormField(
+                              controller: _textController,
+                              focusNode: _focusNode,
+                              textAlignVertical: TextAlignVertical.center,
+                              keyboardType: TextInputType.multiline,
+                              maxLines: 5,
+                              minLines: 1,
+                              onTap: () {
+                                if (_emojiShowing)
+                                  setState(() => _emojiShowing = false);
+                              },
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: "Type a message",
+                                prefixIcon: IconButton(
+                                  onPressed: _toggleEmoji,
+                                  icon: const Icon(Icons.emoji_emotions),
+                                ),
+                                suffixIcon: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {
+                                        showModalBottomSheet(
+                                          backgroundColor: Colors.transparent,
+                                          context: context,
+                                          builder: (builder) => bottomSheet(),
+                                        );
+                                      },
+                                      icon: const Icon(Icons.attach_file),
+                                    ),
+                                    IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.camera_alt),
+                                    ),
+                                  ],
+                                ),
+                                contentPadding: const EdgeInsets.all(5),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 2, left: 2),
+                          child: CircleAvatar(
+                            radius: 25,
+                            backgroundColor: const Color(0xFF128C7E),
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: const Icon(Icons.mic, color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                // 3) Emoji panel ở DƯỚI composer, không chồng lấn
+                Offstage(
+                  offstage:
+                      !_emojiShowing ||
+                      viewInsets > 0, // nếu bàn phím mở thì ẩn emoji
+                  child: SafeArea(
+                    top: false,
+                    left: false,
+                    right: false,
+                    bottom: true, // kê lên trên gesture bar
+                    child: EmojiPicker(
+                      textEditingController: _textController,
+                      scrollController: _emojiScrollController,
+                      config: Config(
+                        height: 280,
+                        checkPlatformCompatibility: true,
+                        emojiViewConfig: EmojiViewConfig(
+                          emojiSizeMax:
+                              28 *
+                              (foundation.defaultTargetPlatform ==
+                                      TargetPlatform.iOS
+                                  ? 1.2
+                                  : 1.0),
+                        ),
+                        // categoryViewConfig: const CategoryViewConfig(
+                        //   initCategory: Category.SMILEYS,
+                        //   recentTabBehavior: RecentTabBehavior.NONE,
+                        // ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
